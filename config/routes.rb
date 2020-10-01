194ca8ea0cd98da1783a07dbeba413ed2f6b50ac
bigBrain1901 <asishaan.191cs124@nitk.edu.in>
2020-10-02 00:51:44 +0530

    Init Commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :posts
+  # root to: 'pages#nccflag'
+
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'pages/home'
+  get 'pages/aims'
+  get 'pages/history'
+  get 'pages/nccsong'
+  get 'pages/motto'
+  get 'pages/nccflag'
+  get 'pages/nccrules' # only for PDFs
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get aims" do
+    get pages_aims_url
+    assert_response :success
+  end
+
+end
