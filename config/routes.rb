269349a9885cbd3d62dcdb2ea6bdf82de22f0391
bigBrain1901 <asishaan.191cs124@nitk.edu.in>
2020-10-04 16:48:26 +0530

    V1 page prep

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
 Rails.application.routes.draw do
   resources :posts
-  # root to: 'pages#nccflag'
+  root to: 'pages#home'
 
   devise_for :users
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  get 'pages/home'
-  get 'pages/aims'
-  get 'pages/history'
-  get 'pages/nccsong'
-  get 'pages/motto'
-  get 'pages/nccflag'
-  get 'pages/nccrules' # only for PDFs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ncc.nitk.ac.in/pages/
+  resources :pages do
+    collection do
+      get :home
+      get :history
+      get :aim_motto
+      get :flag
+      get :song
+      get :alumni
+      get :enrolment
+      get :downloads
+      get :gallery
+      get :about
+    end
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
