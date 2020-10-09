f314ccdd6baba0a5a167b0e921f5dc0db077b720
Pranshu-S <pranshushukla.191me260@nitk.edu.in>
2020-10-10 00:01:47 +0400

    Page Setup + Nav Bar Readt

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_172212) do
+ActiveRecord::Schema.define(version: 2020_03_09_171019) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -24,4 +24,5 @@ ActiveRecord::Schema.define(version: 2020_03_09_172212) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
