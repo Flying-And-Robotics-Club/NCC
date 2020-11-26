7003e19bc3bba0ac0b3fc2e8dc23ea3d99953fab
Pranshu-S <pranshushukla.191me260@nitk.edu.in>
2020-11-26 19:16:56 +0400

    About NCC

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :pages do
     collection do
       get :home
+      get :about
       get :history
       get :aim_motto
       get :flag
@@ -20,7 +21,6 @@ Rails.application.routes.draw do
       get :enrolment
       get :downloads
       get :gallery
-      get :about
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
